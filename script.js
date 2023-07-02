6e39400d0cf2c357025dea5ea0ed000206ee4c32
Brenda Azeredo <135758864+brendafazeredo@users.noreply.github.com>
2023-07-02 17:11:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const boxes = document.querySelectorAll('.boxes > div');
+
+  boxes.forEach(function(box, index) {
+    box.addEventListener('click', function() {
+      if (!box.classList.contains('active')) {
+        boxes.forEach(function(otherBox) {
+          otherBox.classList.remove('active');
+        });
+        box.classList.add('active');
+      }
+    });
+
+    if (index === 0) {
+      box.classList.add('active');
+    }
+  });
+
+  function downloadPDF() {
+    var fileUrl = "https://drive.google.com/file/d/1Y5_iERSYZk0UJG4IX3hTtAm8pEuY8iMi/view?usp=sharing";
+
+    var link = document.createElement("a");
+
+    link.href = fileUrl;
+
+    link.setAttribute("target", "_blank"); // Open link in a new tab
+
+    link.setAttribute("download", "resume.pdf");
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+  }
+
+  const downloadButton = document.querySelector('.download-button');
+  downloadButton.addEventListener('click', downloadPDF);
+});
